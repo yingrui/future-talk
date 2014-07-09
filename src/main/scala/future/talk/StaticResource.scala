6daf4c555400c90bbf761b74261fa7cfb2e30dbf
yingrui <yingrui.f@gmail.com>
2014-07-09 09:10:49 +0800

    add javascript and css files

--- src/main/scala/future/talk/StaticResource.scala
+++ src/main/scala/future/talk/StaticResource.scala
@@ -1,16 +1,35 @@
 package future.talk
 
+import spray.http.StatusCodes
 import spray.routing.HttpService
+import StatusCodes._
 import spray.httpx.marshalling.Marshaller
 
 trait StaticResource extends HttpService {
 
+  val dir = "./src/main/resources"
+
   val staticRoute = {
     pathSingleSlash {
-      getFromResource("index.htm")
+      getFromFile(s"$dir/index.htm")
     } ~
     pathPrefix("css") {
       getFromResourceDirectory("css")
+    } ~
+    pathPrefix("js") {
+      getFromDirectory(s"$dir/js")
+    } ~
+    pathPrefix("fonts") {
+      getFromDirectory(s"$dir/fonts")
+    } ~
+    pathPrefix("images") {
+      getFromDirectory(s"$dir/images")
+    } ~
+    requestUri { uri =>
+      if(uri.path.toString().endsWith(".htm"))
+        getFromDirectory(dir)
+      else
+        complete(NotFound)
     }
   }
 
