bf46e32defa4f423833c9f0f99bf40918cb0bb83
yingrui <yingrui.f@gmail.com>
2014-07-10 23:38:50 +0800

    add a text input in main page

--- src/main/resources/js/app/app.js
+++ src/main/resources/js/app/app.js
@@ -1,2 +1,10 @@
 'use strict';
 
+angular.module('future-talk', []).
+    config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: '/view/main.htm'
+            }).
+        otherwise({redirectTo:'/'});
+}]);
